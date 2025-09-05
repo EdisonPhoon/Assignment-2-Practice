d24f9d774547de1423705f3d5d38179be967a450
Edison Phoon Tek Wei <24FTT2013@pb.edu.bn>
2025-09-05 12:00:38 +0800

    Done custom template (fixed)

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My College Practice</title>
+</head>
+<body>
+    <header>
+        <h1>My Fortress Header</h1>
+    </header>
+
+    <main>
+        <!-- mission content goes here -->
+        @yield('content')
+    </main>
+
+    <footer>
+        My Fortress Footer
+    </footer>
+</body>
+</html>
